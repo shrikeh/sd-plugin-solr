43343925377bb46b541be35cf33b49a3ac07e7c4
Barney Hanlon <barney@shrikeh.net>
2015-12-11 10:46:49 +0000

    Create skeleton

---
+++ Solr.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Server Density Solr Monitoring Plugin
+"""
+
+class Solr(object):
+    def __init__(self, agent_config, checks_logger, raw_config):
+        self.agent_config = agent_config
+        self.logger = checks_logger
+        self.raw_config = raw_config
+
+    def run(self):
+        
